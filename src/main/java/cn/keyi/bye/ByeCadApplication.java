794d0326714e5ed81d25449e36b215fa47af8497
Huang <1012745950@qq.com>
2020-08-05 11:13:03 +0800

    second push

--- src/main/java/cn/keyi/bye/ByeCadApplication.java
+++ src/main/java/cn/keyi/bye/ByeCadApplication.java
@@ -12,7 +12,6 @@ public class ByeCadApplication
 {
     public static void main( String[] args )
     {
-    	System.out.println("111");
     	SpringApplication.run(ByeCadApplication.class, args);
     }
 }
